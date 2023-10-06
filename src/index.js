aa084baf740c55c4df0a0843f80566851d07b874
Anna <kowalewskajaanna@gmail.com>
2023-10-06 21:28:40 +0200

    fix:task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = [];
+  for (let i = 0; i < str.length; i += 1) {
+    switch (str[i]) {
+      case '[': brackets.push('['); break;
+      case '{': brackets.push('{'); break;
+      case '(': brackets.push('('); break;
+      case '<': brackets.push('<'); break;
+      case ']': if (brackets.at(-1) === '[') brackets.pop();
+      else return false; break;
+      case '}': if (brackets.at(-1) === '{') brackets.pop();
+      else return false; break;
+      case ')': if (brackets.at(-1) === '(') brackets.pop();
+      else return false; break;
+      case '>': if (brackets.at(-1) === '<') brackets.pop();
+      else return false; break;
+      default: return false;
+    }
+  }
+  if (brackets.length === 0) return true;
+  return false;
 }
